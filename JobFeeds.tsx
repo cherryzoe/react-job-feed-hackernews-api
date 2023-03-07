b4c9aa53568b925b916f27b20e3fccb79f3dd72e
cherryzoe <jamie.zz815@gmail.com>
2023-03-06 21:31:55 -0800

    4 files modified

--- JobFeeds.tsx
+++ JobFeeds.tsx
@@ -1,7 +1,8 @@
 import React, { use, useEffect, useState } from 'react';
 import styles from './JobFeed.module.css';
 import axios from 'axios';
-import moment from 'moment';
+// import moment from 'moment';
+import { format } from 'date-fns';
 
 const JobFeed = () => {
   const API_FETCH_JOB_IDS = `https://hacker-news.firebaseio.com/v0/jobstories.json`;
@@ -16,57 +17,69 @@ const JobFeed = () => {
   useEffect(() => {
     fetchAllJobIds();
     console.log('useEffect');
-  }, []);
+  }, [pageNumber]);
 
   const fetchAllJobIds = async () => {
-    const response = await axios.get(API_FETCH_JOB_IDS);
-    const jobIds = response.data;
-    // setJobIds(jobIds);
-    //pagenation
     if (!isEnd) {
+      const response = await axios.get(API_FETCH_JOB_IDS);
+      const jobIds = response.data;
+
+      //pagenation
       const splitedJobIds = jobIds.splice(pageNumber * perPage, perPage);
       fetchJobByIDs(splitedJobIds);
-      // console.log(splitedJobIds);
-      // setPageNumber(pageNumber + 1);
-    }
 
-    if (postList.length >= jobIds.length) {
-      setIsEnd(true);
+      if (postList.length >= jobIds.length) {
+        setIsEnd(true);
+      }
     }
   };
 
   const fetchJobByIDs = (ids) => {
-    console.log(ids);
+    // console.log(ids);
 
     ids.forEach((id) => {
-      axios.get(`${API_FETCH_JOB_CONTENT}/${id}.json`).then((response) => {
-        console.log(response.data);
+      axios
+        .get(`${API_FETCH_JOB_CONTENT}/${id}.json`)
+        .then((response) => {
+          console.log(response.data);
 
-        setPostList((postList) => [...postList, response.data]);
-      });
+          setPostList((postList) => [...postList, response.data]);
+        })
+        .catch((err) => console.log(err));
     });
-    console.log(postList);
   };
 
   const Post = ({ post }) => {
+    const date = new Date(post.time);
+    console.log(post.time);
     return (
       <a href={post?.url} target="_blank" style={{ TextDecoder: 'none' }}>
         <div className={styles.postContainer}>
           <p>{post.id}</p>
           <h1>{post.title}</h1>
-          <p>{moment(post?.time).format('Do MMM YYYY hh:mm a')}</p>
+          <p>{format(date, 'MMMM do, yyyy')}</p>
         </div>
       </a>
     );
   };
 
   return (
-    <div>
+    <div className={styles.allPostsContainer}>
       <div className={styles.allPostsContainer}>
         {postList.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      {!isEnd && (
+        <button
+          className={styles.button}
+          onClick={() => {
+            setPageNumber(pageNumber + 1);
+          }}
+        >
+          Load More jobs
+        </button>
+      )}
     </div>
   );
 };
