8f8963005ad8e3682193b8305c7853b4b1275ae0
cherryzoe <jamie.zz815@gmail.com>
2023-03-06 20:52:30 -0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import './style.css';
+import styles from './JobFeed.module.css';
 import JobFeed from './JobFeeds';
 
 export default function App() {
   return (
     <div className="container">
-      <h1 className="header">Hackernews Jobs</h1>
+      <h1 className={styles.header}>Hackernews Jobs</h1>
       <JobFeed />
     </div>
   );
--- JobFeeds.tsx
+++ JobFeeds.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import styles from './JobFeed.module.css';
 import axios from 'axios';
 import moment from 'moment';
@@ -9,36 +9,62 @@ const JobFeed = () => {
 
   const [postList, setPostList] = useState([]);
   const [jobIds, setJobIds] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [isEnd, setIsEnd] = useState(false);
+  const [perPage, setPerPage] = useState(5);
+
+  useEffect(() => {
+    fetchAllJobIds();
+    console.log('useEffect');
+  }, []);
 
   const fetchAllJobIds = async () => {
     const response = await axios.get(API_FETCH_JOB_IDS);
     const jobIds = response.data;
-    fetchJobByIDs(jobIds);
+    // setJobIds(jobIds);
+    //pagenation
+    if (!isEnd) {
+      const splitedJobIds = jobIds.splice(pageNumber * perPage, perPage);
+      fetchJobByIDs(splitedJobIds);
+      // console.log(splitedJobIds);
+      // setPageNumber(pageNumber + 1);
+    }
+
+    if (postList.length >= jobIds.length) {
+      setIsEnd(true);
+    }
   };
 
-  const fetchJobByIDs = (jobIds) => {
-    jobIds.forEach((id) => {
+  const fetchJobByIDs = (ids) => {
+    console.log(ids);
+
+    ids.forEach((id) => {
       axios.get(`${API_FETCH_JOB_CONTENT}/${id}.json`).then((response) => {
-        setPostList((posts) => [...posts, response.data]);
+        console.log(response.data);
+
+        setPostList((postList) => [...postList, response.data]);
       });
     });
+    console.log(postList);
   };
 
-  useEffect(() => {
-    fetchAllJobIds();
-  }, []);
+  const Post = ({ post }) => {
+    return (
+      <a href={post?.url} target="_blank" style={{ TextDecoder: 'none' }}>
+        <div className={styles.postContainer}>
+          <p>{post.id}</p>
+          <h1>{post.title}</h1>
+          <p>{moment(post?.time).format('Do MMM YYYY hh:mm a')}</p>
+        </div>
+      </a>
+    );
+  };
 
   return (
     <div>
       <div className={styles.allPostsContainer}>
         {postList.map((post) => (
-          <a href={post?.url} target="_blank" style={{ TextDecoder: 'none' }}>
-            <div className={styles.postContainer}>
-              <p>{post.id}</p>
-              <h1>{post.title}</h1>
-              <p>{moment(post?.time).format('Do MMM YYYY hh:mm a')}</p>
-            </div>
-          </a>
+          <Post key={post.id} post={post} />
         ))}
       </div>
     </div>
